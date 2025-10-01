7b37bde9dbd6229f59af49cea42cb460948bba08
Manuel Francisco Moreno Durán <158637222+kirito-astumaku@users.noreply.github.com>
2025-10-01 17:34:14 -0600

    cyhjdftyhdstrh

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ export const MONGODB_URI =
   process.env.MONGODB_URI || 'mongodb+srv://mfmoreno092000:Manuel123+@leon708.dkvcj.mongodb.net/';
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://basededatos-production-66c6.up.railway.app";
