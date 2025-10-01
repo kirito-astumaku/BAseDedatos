11fa0c2e657f1c4bfde12d8e15c4efe5d5340882
Manuel Francisco Moreno Durán <158637222+kirito-astumaku@users.noreply.github.com>
2025-10-01 16:02:59 -0600

    aa

--- src/Routes/tasks.routers.js
+++ src/Routes/tasks.routers.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import {createTask,deleteTask,getTask,getTasks,updateTask,} from "../Controllers/tasks.controller.js";
 import { auth } from "../Middlewares/auth.meddleware.js";
 import { validateSchema } from "../Middlewares/validator.middlewre.js";
-import { createTaskSchema } from "../schemas/task.schema.js";
+import { createTaskSchema } from "../schemas/Task.schema.js";
 
 const router = Router();
 
