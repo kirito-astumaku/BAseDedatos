254cdaabc8d29a6e00bacdefe590510dad417049
Manuel Francisco Moreno Durán <158637222+kirito-astumaku@users.noreply.github.com>
2025-10-01 16:33:30 -0600

    hjdty

--- src/app.js
+++ src/app.js
@@ -18,14 +18,6 @@ app.use(cookieParser());
 app.use('/api', authRoutes);
 
 
-if (process.env.NODE_ENV === "production") {
-  const path = await import("path");
-  app.use(express.static("client/dist"));
-
-  app.get("*", (req, res) => {
-    console.log(path.resolve("client", "dist", "index.html") );
-    res.sendFile(path.resolve("client", "dist", "index.html"));
-  });}
 
 
 export default app;
