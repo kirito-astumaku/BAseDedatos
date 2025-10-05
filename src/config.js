4e9acab1638e719b60ece9bd201c11b70bd66ffd
Manuel Francisco Moreno Durán <158637222+kirito-astumaku@users.noreply.github.com>
2025-10-05 12:44:06 -0600

    espera

--- src/config.js
+++ src/config.js
@@ -3,4 +3,4 @@ export const MONGODB_URI =
   process.env.MONGODB_URI || 'mongodb+srv://mfmoreno092000:Manuel123+@leon708.dkvcj.mongodb.net/';
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret";
 
-export const FRONTEND_URL = process.env.FRONTEND_URL || "https://fronten-jet.vercel.app/";
+export const FRONTEND_URL = process.env.FRONTEND_URL || "https://fronten-jet.vercel.app";
